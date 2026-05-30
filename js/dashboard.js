e8ccef5611be4614697934691251032f110770ee
aldijatimulya <aldijatimulya@gmail.com>
2026-05-30 10:53:32 +0700

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,16 @@
+function updateDashboard(){
+
+const provinsi =
+document.getElementById("provinsi").value;
+
+const tahun =
+document.getElementById("tahun").value;
+
+alert(
+"Menampilkan data " +
+provinsi +
+" Tahun " +
+tahun
+);
+
+}
